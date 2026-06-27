fd742f242b54557d5c0fffe3255d53188c1c7c8c
Devyn Miller <144622580+devyn-miller@users.noreply.github.com>
2026-06-27 09:06:58 -0700

    Add: climbing-stairs - submission-7

---
+++ Data Structures & Algorithms/climbing-stairs/submission-7.py
@@ -0,0 +1,13 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        memo = [-1]*(n+1)
+        memo[1],memo[2]=1,2
+        for i in range(3,n+1):
+            memo[i] = memo[i-1] + memo[i-2]
+        return memo[n]
+
+        
