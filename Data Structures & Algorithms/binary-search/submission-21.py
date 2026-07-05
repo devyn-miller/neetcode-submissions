2a6fe07b5bed58bfc7e14ad1bd5e74a1a8e5e720
Devyn Miller <144622580+devyn-miller@users.noreply.github.com>
2026-07-05 07:33:24 -0700

    Add: binary-search - submission-21

---
+++ Data Structures & Algorithms/binary-search/submission-21.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = l + ((r-l)//2)
+            if nums[m] == target:
+                return m
+            elif nums[m] > target:
+                r = m - 1
+            else:
+                l = m + 1
+        return -1
